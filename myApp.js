b1d10c96af0c0366aa31083acd7f4e8883294a54
javlocan <javlocan.dev@gmail.com>
2023-08-30 09:18:36 +0200

    Hello World

--- myApp.js
+++ myApp.js
@@ -1,13 +1,18 @@
 let express = require("express");
+
 let app = express();
 
 homeAbsolutePath = __dirname + "/views/index.html";
+publicAbsolutePath = __dirname + "/public";
 
 app.get("/", (req, res) => {
   res.sendFile(homeAbsolutePath);
 });
-app.get("/", (req, res) => {
+
+app.use(express.static(publicAbsolutePath));
+
+/* app.get("/", (req, res) => {
   res.send("Hello Express");
-});
+}); */
 
 module.exports = app;
